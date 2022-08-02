4c4d982836e592e36a71503db51d8d327657d828
RaeHoon Baek <bmp117@naver.com>
2022-08-02 21:18:05 +0900

    Diary 예제 - 기본 UI 그리기 - 02, 03

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 백래훈 on 2022/08/02.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
