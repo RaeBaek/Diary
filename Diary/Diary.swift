0263bf8c987a2bafff03dd2534f456c7596d9417
RaeHoon Baek <bmp117@naver.com>
2022-08-03 18:18:00 +0900

    Diary 예제 - 일기장 기능 구현하기(일기 작성)

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 백래훈 on 2022/08/02.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
