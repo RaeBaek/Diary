5fc45b1b1b470e1da9ea022233283378c0110f2b
RaeHoon Baek <bmp117@naver.com>
2022-08-08 19:54:28 +0900

    Diary 예제 - 일기장 기능 구현하기(에러처리)

--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -90,17 +90,27 @@ class WriteDiaryViewController: UIViewController {
         
         switch self.diaryEdiorMode {
         case .new:
-            let diary = Diary(title: title, contents: contents, date: date, isStar: false)
+            let diary = Diary(
+                uuidString: UUID().uuidString,
+                title: title,
+                contents: contents,
+                date: date,
+                isStar: false
+            )
             self.delegate?.didSelectRegister(diary: diary)
             
         case let .edit(indexPath, diary):
-            let diary = Diary(title: title, contents: contents, date: date, isStar: diary.isStar)
+            let diary = Diary(
+                uuidString: UUID().uuidString,
+                title: title,
+                contents: contents,
+                date: date,
+                isStar: diary.isStar
+            )
             NotificationCenter.default.post(
                 name: NSNotification.Name("editDiary"),
                 object: diary,
-                userInfo: [
-                    "indexPath.row": indexPath.row
-                ]
+                userInfo: nil
             )
         }
         
